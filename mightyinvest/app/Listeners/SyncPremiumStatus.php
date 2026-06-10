6d1928becbddab3201ab49425f5d83558953c5ab
Yigit Sozer <yigit.3f3@gmail.com>
2026-06-11 01:11:09 +0200

    stripe implementation

--- mightyinvest/app/Listeners/SyncPremiumStatus.php
+++ mightyinvest/app/Listeners/SyncPremiumStatus.php
@@ -20,6 +20,27 @@ class SyncPremiumStatus
      */
     public function handle(object $event): void
     {
-        //
+        $payload = $event->payload;
+
+        \Log::info($payload);
+        $type = $payload['type'] ?? '';
+        
+        if(in_array($type, [
+            'customer.subscription.created',
+            'customer.subscription.updated',
+            'customer.subscription.deleted',
+        ])){
+            $stripeID =$payload['data']['object']['customer'] ?? null;
+            
+            if($stripeID){
+                $user = User::where('stripe_id', $stripeID)->first();
+
+                if($user){
+                    $user->update([
+                        'is_premium' => $user->subscribed('default')
+                    ]);
+                }
+            }           
+        }
     }
 }
